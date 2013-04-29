fd2de27274a298511c557a3c8ca7d6c8fab1fab4
Rahul Pandey <rahul.pandey702@gmail.com>
2013-04-28 21:47:19 -0400

    Modified inputFormatter by Rahul

--- inputformatter.sh
+++ inputformatter.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-for file in *.txt
-do
-#echo $file
-movieid=`sed -n 1p $file`
-movieid=${movieid%?}
-#echo $movieid
-sed -i "1,1d" $file
-sed -i 's/.*/&,'$movieid'/g' $file
-done
-
-
+if [ $# -eq 1 ]
+then
+	for file in $1/*.txt
+	do
+		#echo $file
+		movieid=`sed -n 1p $file`
+		movieid=${movieid%?}
+		#echo $movieid
+		sed -i "1,1d" $file
+		sed -i 's/.*/&,'$movieid'/g' $file
+	done
+else
+	echo 'Incorrect number of arguments: ' $#
+	echo '<Usage> ./script <path to movie ratings folder>'
+fi
